a3c2c689d6b332d54e3be3a74ce3e99f967d6f09
annaruizverdaguer <anna.ruiz@wallapop.com>
2023-01-29 18:05:30 +0100

    book component

---
+++ src/Llibre.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+export default function Llibre(props) {
+  let infoLlibre = {
+    titol: props.titol,
+    categoria: props.categoria,
+    any: props.any,
+    pagines: props.pagines,
+    estat: props.estat,
+  };
+}
