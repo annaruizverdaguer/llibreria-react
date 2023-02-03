b7badb1e9c26046f8af95425a5cda56baf45bf22
Anna Ruiz Verdaguer <anna.ruizverdaguer@gmail.com>
2023-02-03 19:53:58 +0100

    Revert "Afegint el formulari. Ara mateix només es fa un console.log() amb el llibre introduït. Següent pas serà passar la info a la llibreria"
    This reverts commit 2743ceebf1a525e8dac0762043bbd61cff5c007a.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import "./App.css";
-import Form from "./Form.js";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Form />
-
-      {/* Aquí tindrem un formulari on l'acció del formulari serà afegir un llibre a una llista */}
-      {/* Per cada element de la llista cridarem a l'element Llibre */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
