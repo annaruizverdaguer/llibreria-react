5903e427160b0ae010e8bb93264a8a94ade75df5
Anna Ruiz Verdaguer <anna.ruizverdaguer@gmail.com>
2023-02-03 19:58:54 +0100

    Afegint el formulari. Ara mateix només es fa un console.log() amb el llibre introduït. Següent pas serà passar la info a la llibreria

---
+++ src/Form.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+export default function Form() {
+
+  const FORM_INITIAL_STATE = {
+    bookTitle: "",
+    bookCategory: "",
+    bookYear: "",
+    bookPages: "",
+    bookPagesRead: "",
+  };
+  const [form, setForm] = useState(FORM_INITIAL_STATE);
+
+  function updateFormField(event) {
+    const formField = event.target.id;
+    setForm({ ...form, [formField]: event.target.value });
+  }
+
+  function addBookToLibrary(event) {
+    event.preventDefault();
+    console.log(form);
+  }
+
+  return (
+    <div>
+      <form onSubmit={addBookToLibrary}>
+        <div>
+          <label htmlFor="bookTitle">Títol: </label>
+          <input
+            type="text"
+            required
+            id="bookTitle"
+            onChange={updateFormField}
+          />
+        </div>
+        <div>
+          <label htmlFor="bookCategory">Categoria: </label>
+          <select id="bookCategory" required onChange={updateFormField}>
+            Sel·lecciona una categoria
+            <option value="accion">Acció i Aventures</option>
+            <option value="art">Arts i Música</option>
+            <option value="science-fiction">Ciència Ficció</option>
+            <option value="classics">Clàssics</option>
+            <option value="comic">Còmics</option>
+            <option value="history">Història</option>
+            <option value="horror">Horror</option>
+            <option value="novel">Novel·la</option>
+            <option value="poetry">Poesia</option>
+            <option value="thriller">Suspens</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="bookYear">Any publicació: </label>
+          <input
+            type="number"
+            required
+            id="bookYear"
+            onChange={updateFormField}
+          />
+        </div>
+        <div>
+          <label htmlFor="bookPages">Nº pàgines: </label>
+          <input
+            type="number"
+            required
+            id="bookPages"
+            onChange={updateFormField}
+          />
+        </div>
+        <div>
+          <label htmlFor="bookPagesRead">% pàgines llegides:</label>
+          <input
+            type="range"
+            required
+            id="bookPagesRead"
+            onChange={updateFormField}
+          />
+        </div>
+        <div>
+          <button type="submit">Guarda el llibre!</button>
+        </div>
+      </form>
+    </div>
+  );
+}
