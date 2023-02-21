8af2ffdfda50e34a55dbbd136fed6a7701c7d1ab
Anna Ruiz Verdaguer <anna.ruizverdaguer@gmail.com>
2023-02-21 20:06:59 +0100

    Adding styles to App file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>LA TEVA LLIBRERIA</h1>
       <Form/>
       {/* Aquí tindrem un formulari on l'acció del formulari serà afegir un llibre a una llista */}
       {/* Per cada element de la llista cridarem a l'element Llibre */}
